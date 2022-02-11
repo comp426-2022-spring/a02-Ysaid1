c09e26ebea5f1fa5671030ad13f83a5edf7c0d6e
Ysaid1 <Ysaid@unc.edu>
2022-02-10 22:53:35 -0500

    try14

--- flips.js
+++ flips.js
@@ -4,9 +4,12 @@ import {coinFlips, countFlips} from "./modules/coin.mjs";
 //Get command line
 const args = minimist(process.argv.slice(2))
 //if no arguement is given, flip once
-if (args.number == undefined || args.number == null){
+if (args.number == undefined){
     //set the number of arguements to one
     args.number = 1;
+}
+if (args.number == null || args.number ==''){
+    console.log("Error: no input.");
 }
 //create an array to hold the number of flips given by command line
 var arrayForFlips = coinFlips(args.number);
