f09542e9fa7d393d01b688a1b1b56d10d6ea8241
Ysaid1 <Ysaid@unc.edu>
2022-02-11 12:07:57 -0500

    Made some changes to flips

--- flips.js
+++ flips.js
@@ -2,18 +2,18 @@ import minimist from 'minimist';
 //Importing the functions
 import {coinFlips, countFlips} from "./modules/coin.mjs";
 //Get command line
-const args = minimist(process.argv.slice(2))
+const args = minimist(process.argv.slice(2)) || 1;
 //create an array to hold the number of flips given by command line
-var arrayForFlips = coinFlips(args.number);
 //if no arguement is given, flip once
-if(args.number == null){
+if(args.number == null || args.number == ''){
     var oneCoinFlip = coinFlips(1);
     if (oneCoinFlip ==  'tails' ) {
         console.log("{ tails: 1 }");
     } else {
         console.log("{ heads: 1 }");
     }
-}//print out the array of flips and the count of heads and tails
+}
+//print out the array of flips and the count of heads and tails
 else {
     var flipResut =  coinFlips(args.number);
     console.log(flipResut)
