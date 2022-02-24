652d95fc00da070a7afdf9969227b11960e0099a
Ysaid1 <Ysaid@unc.edu>
2022-02-24 11:13:45 -0500

    Fixed error for flips.js

--- flips.js
+++ flips.js
@@ -6,8 +6,8 @@ const args = minimist(process.argv.slice(2)) || 1;
 //if no arguement is given, flip once
 if(args.number == null){
     var oneCoinFlip = coinFlips(1);
-    console.log(flip);
-    if (oneCoinFlip ==  ['tails'] ) {
+    console.log(oneCoinFlip);
+    if (oneCoinFlip ==  ['tails']) {
         console.log('{ tails: 1 }');
     } else {
         console.log('{ heads: 1 }');
@@ -15,7 +15,7 @@ if(args.number == null){
 }
 //print out the array of flips and the count of heads and tails
 else {
-    var flipResut =  coinFlips(args.number);
+    var flipResut = coinFlips(args.number);
     console.log(flipResut)
     console.log(countFlips(flipResut)) 
 }
